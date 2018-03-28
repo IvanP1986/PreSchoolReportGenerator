6e01e02fff083e553f195e7eeaaf2d97d95c1146
ipustobaev <ipustobaev@ozon.ru>
2018-03-28 14:31:01 +0300

    #create solution

---
+++ Utilities/Period.cs
@@ -0,0 +1,17 @@
+﻿namespace Utilities
+{
+    /// <summary>
+    /// Период отчетности.
+    /// </summary>
+    public class Period
+    {
+        /// <summary>
+        /// Год.
+        /// </summary>
+        public int Year { get; set; }
+        /// <summary>
+        /// Месяц.
+        /// </summary>
+        public int Month { get; set; }
+    }
+}
---
+++ Utilities/Report.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Utilities
+{
+    /// <summary>
+    /// Отчет о посещаемости ребенка.
+    /// </summary>
+    public class Report
+    {
+        /// <summary>
+        /// Полное имя ребенка.
+        /// </summary>
+        public string FullName { get; set; }
+        /// <summary>
+        /// Период.
+        /// </summary>
+        public Period Period { get; set; }
+        /// <summary>
+        /// Возрастная группа.
+        /// </summary>
+        public string AgeGroup { get; set; }
+    }
+}
